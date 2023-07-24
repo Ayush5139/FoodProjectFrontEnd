0a0bb0dbddbec4468875685a81a23e3b5defff95
Ayush5139 <2018.ayush.sachdev@ves.ac.in>
2023-07-24 15:37:47 +0530

    updated 24/07/23

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,23 +1,38 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import Nav from './Nav'
 import './profilepage.css'
 
 function ProfilePage() {
   const userid = sessionStorage.getItem("userid")
-  const [username,setUsernaem] = useState("")
-  useEffect(()=>{
+  const [username, setUsernaem] = useState("")
+  const [titleformap, setTiteformap] = useState("")
+  useEffect(() => {
     axios.get(`https://food-backend-gub7.onrender.com/getuserdetails/${userid}`)
-    .then((res)=>setUsernaem(res.data))
+      .then((res) => setUsernaem(res.data))
   })
+  console.log(username)
+  const [reviews, setReviews] = useState([])
+  function getUserReviews() {
+    axios.get(`https://food-backend-gub7.onrender.com/getuserrviews/${username}`)
+      .then((res) => setReviews(res.data))
+    setTiteformap("Reviews")
+  }
+  function getUserQuestions() {
+    axios.get(`https://food-backend-gub7.onrender.com/getuserques/${username}`)
+      .then((res) => setReviews(res.data))
+    setTiteformap("Questions")
+  }
+  console.log(reviews)
   return (
     <div>
-      <Nav/>
+      <Nav />
       <div className='profilecoverphoto'>
       </div>
       <div className='mainprofile'>
         <div className='profileitems'>
-          <img src="https://img-nonprod.sndimg.com/food/image/upload/w_202,h_202,q_80,c_fill,fl_progressive/v1/qa1/fdc/img/placeholder/avatar.jpg" className='profileimg'/>
+          <img src="https://img-nonprod.sndimg.com/food/image/upload/w_202,h_202,q_80,c_fill,fl_progressive/v1/qa1/fdc/img/placeholder/avatar.jpg" className='profileimg' />
           <p className='emailtag'>@{username}</p>
         </div>
         <div className='datediv'>
@@ -25,13 +40,25 @@ function ProfilePage() {
         </div>
       </div>
       <div className='extraprofilediv'>
-        <div className=''>
-          <ul>
-
+        <div className='listdiv'>
+          <ul type="none">
+            <li onClick={getUserQuestions}>Questions</li>
+            <li onClick={getUserReviews}>Reviews</li>
           </ul>
         </div>
+        <div className='profilemapdiv'>
+          <h1>{titleformap}</h1>
+          {
+            reviews.map((item) => (
+              <div className='profilemapping'>
+                <p>{item.Review}</p>
+                <Link to={`/item/${item.RecipeId}`} className="profilemaplink">{item.RecipeName}</Link>
+              </div>
+            ))
+          }
+        </div>
       </div>
-    </div>  
+    </div>
   )
 }
 
